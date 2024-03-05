aa03e4fbbdca8c1066c5dcc2fc019da064b9e1b0
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-03-05 17:15:17 +0000

    moved around the folder structure

--- src/app.py
+++
@@ -1,7 +0,0 @@
-"""This module provides the main functionality of the application.
-
-Mainly used for debugging and developing the interface and functionality
-"""
-import streamlit as st
-
-st.write("sup dawg")
